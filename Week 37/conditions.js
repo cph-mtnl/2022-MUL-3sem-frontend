7df9633784db588a02f910550337cf539613300d
cph-mtnl <mtnl@cphbusiness.dk>
2022-09-13 08:55:31 +0200

    code for lecture start

---
+++ Week 37/conditions.js
@@ -0,0 +1,24 @@
+//Tænk over variabel og funktions navne
+
+// Øvelse 1.1: (Conditions)
+// Lav en funktion,
+// der fortæller om en input string er minimum 3 karakterer.
+
+// Øvelse 1.2: (Logical operators)
+// Udvid funktionen med at den maksimum må være 8 karakterer.
+
+// Øvelse 1.3:
+// Lav en funktion, om en string indeholder "cphbusiness.dk"
+
+// Øvelse 1.4:
+// Lav en funktion om 2 strings matcher.
+
+// Øvelse 2.1:
+// Lav en event listener på "submit" og indsamle værdierne.
+
+// Øvelse 2.2:
+// Sammensæt de tidligere funktioner med det nye event.
+// Lav nogle if sætninger afhængig af resultatet af de enkelte funktioner
+
+// Øvelse 2.3:
+// Tilføj en klasse til input feltet, hvis det ikke valideres.
